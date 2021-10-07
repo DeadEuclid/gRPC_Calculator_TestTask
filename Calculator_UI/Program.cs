6e71a2643c71257d67738ef8bfce40e17addff72
DeadEuclid <60969774+DeadEuclid@users.noreply.github.com>
2021-10-07 15:29:22 +0700

    Добавьте файлы проекта.

---
+++ Calculator_UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculator_UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
